38d7c1e562447f9098b9f5ccd0e2817075cf6c5f
emilia <ahmerovaemilia@yandex.ru>
2024-05-11 12:50:29 -0700

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ахмерова Эмилия on 11.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
